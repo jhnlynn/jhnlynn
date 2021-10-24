91cd28f403ce193e3af5aeca4bc6545fa0f9e0d3
johannes-lin <jl12183@nyu.edu>
2021-10-24 10:58:02 -0400

    :poop: try Blockquotes for markdown src paragraph wrap

--- index.js
+++ index.js
@@ -150,10 +150,10 @@ ${toolsTitle}\n
 <p align="left">\n
     ${tools}\n
 </p>\n
-${`<!--START_SECTION:waka-->`}\n
-${` `}\n
-${` `}\n
-${`<!--END_SECTION:waka-->`}\n
+${`> <!--START_SECTION:waka-->`}\n
+${`> `}\n
+${`> `}\n
+${`> <!--END_SECTION:waka-->`}\n
 <br>\n
 ${stats}\n
 ${visitor_counter}\n
